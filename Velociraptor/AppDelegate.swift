77a3641305d9fd437af4a237fa8e23503e449ca2
Vegard Skui <me@vegardskui.com>
2020-06-06 11:24:39 +0200

    Request when in use location authorization

--- Velociraptor/AppDelegate.swift
+++ Velociraptor/AppDelegate.swift
@@ -6,15 +6,18 @@
 //  Copyright © 2020 Vegard Skui. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+
         return true
     }
 
@@ -30,5 +33,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Location authorization status changed, new status: \(status.rawValue)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last!
+
+        print(lastLocation)
     }
 }
