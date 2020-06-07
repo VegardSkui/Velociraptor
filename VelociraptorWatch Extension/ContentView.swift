3f90f27b498e9735f5357487704eba85fad5d1a2
Vegard Skui <me@vegardskui.com>
2020-06-07 12:52:49 +0200

    Add independent watch app

---
+++ VelociraptorWatch Extension/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  VelociraptorWatch Extension
+//
+//  Created by Vegard Skui on 07/06/2020.
+//  Copyright © 2020 Vegard Skui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var appState: AppState
+    @EnvironmentObject var locationManager: LocationManager
+
+    @ViewBuilder
+    var body: some View {
+        if locationManager.hasAuthorization {
+            VStack {
+                Text(String(format: "%.1f", appState.unitOfSpeed.convert(speed: locationManager.location.speed)))
+                    .font(.largeTitle)
+                Text(appState.unitOfSpeed.symbol)
+            }.onTapGesture {
+                self.appState.unitOfSpeed.next()
+            }
+        } else {
+            Text("The app needs access to your location to determine your current speed")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(AppState())
+            .environmentObject(LocationManager())
+    }
+}
